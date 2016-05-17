f4a84e9bccbcb917f7470a111f2314b43e53b14e
anaisM <anais-massardier@hotmail.fr>
2016-05-17 11:16:44 +0200

    correction link connexion/deco

--- header.php
+++ header.php
@@ -42,13 +42,13 @@
 					{
 				?>
 						<!-- si l'internaute n'est pas connecté, il peut se connecter ou s'inscrire -->
-						<span><a href="./include/connexion.php">Se connecter</a> | <a href="./signup_form.php">S'inscrire</a></span> 
+						<span><a href="./include/connexion.php">Se connecter</a> | <a href="./include/register.php">S'inscrire</a></span> 
 
 				<?php
 					} else {
 				?>
 						<!-- si le membre est connecté il peut voir son profil ou se deconnecter -->
-						<span><a href="./profil_view_edit.php?id_membre=<?php echo $_SESSION["id_membre"];?>">Mon compte</a> | <a href="deconnexion.php">Se déconnecter</a></span> 
+						<span><a href="./profil_view_edit.php?id_membre=<?php echo $_SESSION["id_membre"];?>">Mon compte</a> | <a href="include/logout.php">Se déconnecter</a></span> 
 				<?php } ?>
 			</div>
 		</div> <!-- row -->		
