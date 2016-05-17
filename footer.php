680d0a55f26f061e136b6d80ccf06c7eac6a1b4b
anaisM <anais-massardier@hotmail.fr>
2016-05-17 11:04:49 +0200

    Footer, jquery, doctype et bootstrap

---
+++ footer.php
@@ -0,0 +1,60 @@
+﻿<div class="row"> <!-- container du pied de page -->
+	<div id="footer" class="clearfix">
+		<div id="footer-widgets" >
+		﻿	<div id="footer-wrapper" > <!-- 1ere partie du footer, contient le logo et quelques liens -->
+				<div class="row">
+					<div class="col-sm-6 col-md-3"> <!-- logo de l'universite et de polytech -->
+						<div id="meta-3" class="widget widgetFooter widget-meta">
+							<div class="row">
+								<img src="./images/logo_universite.png" alt="logo-universite" />
+							</div>				
+							<div class="row">
+								<img src="./images/logo_polytech.png" alt="logo-polytech" />
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-3"> <!-- liens liés au compte, si connecté : voir son profil ou se déconnecter, sinon se connecter ou inscription -->
+						<div id="meta-3" class="widget widgetFooter widget-meta">
+							<h4 class="widgettitle">COMPTE</h4>
+							<ul>
+								<?php if(!isset($_SESSION["id_membre"])) { ?>
+									<li><a href="./include/connexion.php">Se connecter</a></li>
+									<li><a href="./signup_form.php">Inscription</a></li>
+								<?php }else {?>
+									<li><a href="./profil_view_edit.php?id_membre=<?php echo $_SESSION["id_membre"]; ?>">Mon compte</a></li>
+									<li><a href="./deconnexion.php">Se déconnecter</a></li>
+								<?php } ?>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-3"> <!-- Liens lié au site : accueil, page a propos et page de contact -->
+						<div id="meta-3" class="widget widgetFooter widget-meta">
+							<h4 class="widgettitle">INFORMATIONS</h4>
+							<ul>
+								<li><a href="./index.php">Accueil</a></li>
+								<li><a href="./a_propos.php">A propos</a></li>
+								<li><a href="./contact.php">Nous contacter</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-3"><!-- logo de polyquest -->
+						<div id="meta-3" class="widget widgetFooter widget-meta">
+							<img src="./images/logo.png" alt="logo" />
+						</div>
+					</div>
+				</div> <!-- row-->
+			</div> <!-- footer-wrapper -->
+			<div id="sub-floor"> <!-- seconde partie du pied de page copyright et noms des étudiants -->
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4 copyright">Copyright © PolyQuest -2016- Tous droits réservés</div>
+						<div class="col-md-4 col-md-offset-4 attribution">BRECHENMACHER Grégoire | CLAUDET Adrien | CORDEAU Elyse <br/> EDORH TOSSA Léon | HELIES Elsa | MASSARDIER Anaïs | NGO BUI HUNG Christelle </div>
+					</div>
+				</div>
+			</div> <!-- row-->
+		</div>	<!-- footer -->
+	</div>
+</div>
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,28 +1,57 @@
-<!-- En tête du site à inclure sur les autres pages -->
-<div class="header">
-	<div class="row">
-		<div class="col-lg-8"> <!-- logo du site avec lien vers l'accueil -->
-			<a href="./index.php"><img class="logo" src="./ressources/images/logo.jpg" alt="logo"/></a>
-		</div>
-
-		<div class="col-lg-4"> <!-- lien du compte -->
-			<?php
-				if (!isset($_SESSION['id_membre']))
-				{
-			?>
-					<!-- si l'internaute n'est pas connecté, il peut se connecter ou s'inscrire -->
-					<span><a href="./connexion.php">Se connecter</a> | <a href="./signup_form.php">S'inscrire</a></span> 
-
-			<?php
-				} else {
-			?>
-					<!-- si le membre est connecté il peut voir son profil ou se deconnecter -->
-					<span><a href="./profil_view_edit.php?id_membre=<?php echo $_SESSION["id_membre"];?>">Mon compte</a> | <a href="deconnexion.php">Se déconnecter</a></span> 
-			<?php } ?>
-		</div>
-	</div> <!-- row -->		
-	
-	<div class="row">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php session_start(); ?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<!-- Le jeu de caractères de la page (encodage) -->
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		
-	</div>
-</div> <!-- header --> 
+		<!-- Pour le navigateur, l'historique et les moteurs de recherche -->
+		<title>PolyQuest</title>
+		
+		<!-- Pour les moteurs de recherche -->
+		<meta name="description" lang="fr" content="jeu de combat en ligne"/>
+		<meta name="keywords" lang="fr" content="game, polytech, online"/>
+		
+		<!-- Liens vers le css et les scripts pour la selection des dates -->
+		<link rel="stylesheet" type="text/css" href="./include/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="./include/bootstrap/css/custom.css">
+		
+		<!-- font -->
+		<!--<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Marck+Script' rel='stylesheet' type='text/css'>-->
+		
+		<!-- Liens pour l'utilisation de JQuery -->
+		<link rel="stylesheet" href="./include/jquery-ui-1.11.4/jquery-ui.css">
+		<script type="text/javascript" src="./include/jquery-1.11.2.js" ></script>
+		<script src="./include/jquery-ui-1.11.4/jquery-ui.js"></script>
+		<script src="./include/bootstrap/js/bootstrap.min.js"></script>
+		
+	</head>
+		
+	<!-- En tête du site à inclure sur les autres pages -->
+	<div class="header">
+		<div class="row">
+			<div class="col-lg-8"> <!-- logo du site avec lien vers l'accueil -->
+				<a href="./index.php"><img class="logo" src="./images/logo.png" alt="logo"/></a>
+			</div>
+
+			<div class="col-lg-4"> <!-- lien du compte -->
+				<?php
+					if (!isset($_SESSION['id_membre']))
+					{
+				?>
+						<!-- si l'internaute n'est pas connecté, il peut se connecter ou s'inscrire -->
+						<span><a href="./include/connexion.php">Se connecter</a> | <a href="./signup_form.php">S'inscrire</a></span> 
+
+				<?php
+					} else {
+				?>
+						<!-- si le membre est connecté il peut voir son profil ou se deconnecter -->
+						<span><a href="./profil_view_edit.php?id_membre=<?php echo $_SESSION["id_membre"];?>">Mon compte</a> | <a href="deconnexion.php">Se déconnecter</a></span> 
+				<?php } ?>
+			</div>
+		</div> <!-- row -->		
+		
+		
+	</div> <!-- header --> 
