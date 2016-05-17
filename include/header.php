6992de3395e1e91b393c5504015f44ca211848d7
DrRipper <greg.brech@hotmail.fr>
2016-05-17 15:32:03 +0200

    Update header.php

--- include/header.php
+++ include/header.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<?php session_start(); ?>
+<?php 
+	if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    }  
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<!-- Le jeu de caractères de la page (encodage) -->
