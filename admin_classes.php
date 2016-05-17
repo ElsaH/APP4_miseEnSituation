ba9e321eb5a3c4bda2bc7297e7be73029acc9936
DrRipper <greg.brech@hotmail.fr>
2016-05-17 16:01:37 +0200

    gestion des classes

---
+++ admin_classes.php
@@ -0,0 +1,100 @@
+<?php
+
+include("include/header.php");
+include("include/connexionBD.php");
+
+if(!isset($_SESSION["admin"]) || !$_SESSION["admin"]) {
+	header('HTTP/1.0 403 Not Found');
+	echo "<h1>403 Access Denied/Forbidden</h1>";
+	echo "Vous ne disposez pas des droits suffisants pour acceder a cette page";
+	exit();
+}
+
+?>
+
+<body>
+	<div class='row'>
+		<div class='col-md-8'>
+		<h1>Gestion des classes</h1>
+			<div>
+		  		<table id="table_classes" class="table table-striped table-hover">
+	                <thead>
+	                	<tr>
+		                    <th>ID Absence</th>
+		                    <th>ID Salarié</th>
+		                    <th>Salarié</th>
+		                    <th>Type Absence</th>
+		                    <th>Date début</th>
+		                    <th>Date fin</th>
+	                	</tr>
+	                </thead>
+	            </table>
+	        </div>
+	    </div>
+	</div>
+</body>
+				
+				<!--<table class="table table-striped">
+					<thead> <!-en tête de la table des classes 
+					  <tr>
+						<th>Nom de classe</th>
+						<th>Email</th>
+						<th>Action</th>
+					  </tr>
+					 
+					</thead>
+					
+					<tbody> <!-contenu de la table 
+					<?php
+						/*try{
+							//===== On va chercher tous les utilisateurs =====
+							$rqt_users = "SELECT * FROM user ORDER BY pseudo ";
+							
+							//==== connexion a la base de données ====
+							$db = connexion();
+							
+							$stmt = $db->prepare($rqt_users);
+							$stmt->execute();
+							$result = $stmt->fetchAll();
+							
+							if(is_array($result)){	
+								foreach($result as $row ) {	
+									//==== Info de l'utilisateur
+									$iduser = $row["id_user"];
+									$idemail = $row["mail_user"];
+									$idpseudo = $row["pseudo"];
+									$admin = $row["admin"];
+									if ($admin == 1){$activite = 'disabled="disabled"';}
+									else $activite = "";
+					?>
+									<tr id=<?= $iduser ?> >
+										<td> <?= $idpseudo ?> </td>
+										<td> <?= $idemail ?> </td>
+										
+										<td> <!-- Les actions (supprimer, modifier ou contacter) 
+											<button class="btn btn-danger" type="button" <?= $activite ?> onclick='suppression("<?= $idemail?>","<?=$iduser?>")'> <span class="glyphicon glyphicon-remove"></span> </button>
+											<button class="btn btn-primary" type="button" onclick="location.href='profil_view_edit.php?id_membre=<?= $iduser ?>';"> <span class="glyphicon glyphicon-pencil"></span> </button>
+											<a href="mailto:<?= $idemail ?>" class="btn btn-info" type="button"><span class="glyphicon glyphicon-envelope"></span></a>
+										</td>
+									</tr>
+									<?php
+									}
+									} 
+							
+						}catch (PDOException $ex){
+							echo "Problème de connexion MySQL";*/
+						}
+					?> 
+					</tbody>
+				</table>
+			</div> <!- ./tableUsers 
+		</div> <!- ./col 
+	</div> <!- ./row -->
+
+
+<?php
+
+
+include("include/footer.php");
+
+?>
