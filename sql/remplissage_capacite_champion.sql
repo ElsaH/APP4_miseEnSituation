25c454f518888a876e2039f017f1c79234d9480a
leon.e.t <edorh.tossa.leon@gmail.com>
2016-05-18 14:04:06 +0200

    Table SQL
    Script de remplissage des tables capacite, capacite_champion,
    remplissage_champion

---
+++ sql/remplissage_capacite_champion.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM polyquest.champion_capacite
+LIMIT 0, 1000
+
+-- Date: 2016-05-18 14:01
+*/
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (1,1,1);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (2,1,2);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (3,1,3);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (4,2,4);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (5,2,5);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (6,2,6);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (7,3,7);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (8,3,8);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (9,3,9);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (10,4,10);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (11,4,11);
+INSERT INTO `champion_capacite` (`id_champion_capacite`,`id_champion`,`id_capacite`) VALUES (12,4,12);
