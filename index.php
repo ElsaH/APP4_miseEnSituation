3894a5024b8e2b4b06d5cfa6303cc63ba822f740
Tofuw <tofuw.tofuw@gmail.com>
2016-05-18 11:30:46 +0200

    Ajout page guide_du_jeu.php et modification index.php

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 include("./include/header.php");
-echo "Coucou :)";
+echo "Coucou :)<br>";
+echo "REMPLISSAGE REMPLISSAGE<br>";
+echo "REMPLISSAGE REMPLISSAGE<br>";
+echo "REMPLISSAGE REMPLISSAGE<br>";
+echo "REMPLISSAGE REMPLISSAGE<br>";
+echo '<a href="./guide_du_jeu.php">GUIDE DU JEU</a><br>';
+echo "REMPLISSAGE REMPLISSAGE<br>";
+echo "REMPLISSAGE REMPLISSAGE<br>";
+echo "REMPLISSAGE REMPLISSAGE<br>";
+echo "REMPLISSAGE REMPLISSAGE<br>";
+echo "REMPLISSAGE REMPLISSAGE<br>";
+echo "REMPLISSAGE REMPLISSAGE<br>";
 include("./include/footer.php");
 ?>
