5113f3feab312b4a138c947c076acb0bab70aa10
leon.e.t <edorh.tossa.leon@gmail.com>
2016-05-18 14:41:53 +0200

    SQL Table
    Remplissage table user

---
+++ sql/remplissage_user.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM polyquest.user
+LIMIT 0, 1000
+
+-- Date: 2016-05-18 14:39
+*/
+INSERT INTO `user` (`id_user`,`admin`,`pseudo`,`photo`,`password`,`mail_user`,`xp`,`combats_joues`,`combats_gagnes`) VALUES (1,1,'leon',NULL,'leon','leon@gamil.com',20,1,1);
+INSERT INTO `user` (`id_user`,`admin`,`pseudo`,`photo`,`password`,`mail_user`,`xp`,`combats_joues`,`combats_gagnes`) VALUES (2,0,'tofuw',NULL,'tofuw','tofuw@gmail.com',40,2,1);
+INSERT INTO `user` (`id_user`,`admin`,`pseudo`,`photo`,`password`,`mail_user`,`xp`,`combats_joues`,`combats_gagnes`) VALUES (3,0,'adri',NULL,'adri','adri@gmail.com',5,0,0);
+INSERT INTO `user` (`id_user`,`admin`,`pseudo`,`photo`,`password`,`mail_user`,`xp`,`combats_joues`,`combats_gagnes`) VALUES (4,1,'test',NULL,'test','test@gmail.com',0,0,0);
+INSERT INTO `user` (`id_user`,`admin`,`pseudo`,`photo`,`password`,`mail_user`,`xp`,`combats_joues`,`combats_gagnes`) VALUES (5,0,'test2',NULL,'test2','test2@gmail.com',0,0,0);
