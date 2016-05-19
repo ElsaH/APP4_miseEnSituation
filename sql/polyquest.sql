4c942bb545e36d031646b9110b31d21f58bcb968
leon.e.t <edorh.tossa.leon@gmail.com>
2016-05-19 16:48:31 +0200

    MAJ SQL

--- sql/polyquest.sql
+++ sql/polyquest.sql
@@ -32,18 +32,18 @@ CREATE TABLE `capacite` (
 -- ----------------------------
 -- Records of capacite
 -- ----------------------------
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (1,'mug_de_cafe',5,0,10,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (2,'lance_de_clavier',0,10,15,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (3,'riff_de_guitare',0,10,30,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (4,'potion_au_ciment',5,0,10,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (5,'lance_de_disque_en_ceramique',0,10,20,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (6,'projection_de_roche',0,15,30,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (7,'pile_a_combustile',5,0,5,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (8,'morsure_de_cable_electrique',0,10,15,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (9,'electrocution_de_masse',0,15,30,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (10,'lumiere_noire',10,0,15,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (11,'lentille_convergente',0,5,15,0);
-INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (12,'rayon_laser_multiple',0,10,20,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (1,'mug de cafe',5,0,10,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (2,'lance de clavier',0,10,15,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (3,'riff de guitare',0,10,30,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (4,'potion au ciment',5,0,10,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (5,'lance de disque en ceramique',0,10,20,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (6,'projection de roche',0,15,30,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (7,'pile à combustile',5,0,5,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (8,'morsure de cable electrique',0,10,15,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (9,'electrocution de masse',0,15,30,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (10,'lumiere noire',10,0,15,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (11,'lentille convergente',0,5,15,0);
+INSERT INTO `capacite` (`id_capacite`,`nom_capacite`,`montant_soins`,`montant_degats`,`cout_mana`,`xp_requis`) VALUES (12,'rayon laser multiple',0,10,20,0);
 
 -- ----------------------------
 -- Table structure for `champion`
