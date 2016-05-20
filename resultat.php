752f8a09cd8705e4f9af8b3879047d7da13c9a6b
ElsaH <helies.elsa@gmail.com>
2016-05-20 12:13:44 +0200

    mise à  jour résultat et xp

--- resultat.php
+++ resultat.php
@@ -24,6 +24,55 @@ else {
 	$score2 = "gagné";
 }
 
+/* On met à jour l'xp pour le gagné et le nombre de combat */
+
+$joueur1 = $bd->prepare('SELECT * FROM user WHERE pseudo=:perso1');
+$joueur1->execute(array(
+	'perso1' => $perso1
+));
+$joueur1 = $joueur1->fetch();
+
+if($numWin == 0) {
+	$combats_gagnes = $joueur1['combats_gagnes'] +1;
+	$xp = $joueur1['xp'] +1;
+} else {
+	$combats_gagnes = $joueur1['combats_gagnes'];
+		$xp = $joueur1['xp'];
+}
+
+$update1 = $bd->prepare('UPDATE user SET combats_joues = :combats_joue, combats_gagnes = :combats_gagnes, xp = :xp WHERE user.id_user = :id_user;')
+$update1->execute(array(
+	'combats_gagnes' => $combats_gagnes,
+	'combats_joues' =>> ($joueur1['combats_joues'] + 1),
+	'id_user' => $joueur1['id_user'],
+	'xp' => $xp
+)); 
+
+$joueur2 = $bd->prepare('SELECT * FROM user WHERE pseudo=:perso2');
+$joueur2->execute(array(
+	'perso2' => $perso2
+));
+$joueur2 = $joueur2->fetch();
+
+if($numWin == 0) {
+	$combats_gagnes = $joueur2['combats_gagnes'];
+	$xp = $joueur2['xp'];
+} else {
+	$combats_gagnes = $joueur2['combats_gagnes']+1;
+		$xp = $joueur2['xp'] +1;
+}
+
+$update1 = $bd->prepare('UPDATE user SET combats_joues = :combats_joue, combats_gagnes = :combats_gagnes, xp = :xp WHERE user.id_user = :id_user;')
+$update1->execute(array(
+	'combats_gagnes' => $combats_gagnes,
+	'combats_joues' =>> ($joueur2['combats_joues'] + 1),
+	'id_user' => $joueur2['id_user'],
+	'xp' => $xp
+));  
+
+
+/* **/
+
 $joueur1 = $bd->prepare('SELECT * FROM user WHERE pseudo=:perso1');
 $joueur1->execute(array(
 	'perso1' => $perso1
