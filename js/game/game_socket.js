8d03f9ef5f53b53b70cdcd1c7227caceac80de93
Annoa1 <elyse.cordeau@u-psud.fr>
2016-05-20 11:55:46 +0200

    retentative de redirection resultat.php

--- js/game/game_socket.js
+++ js/game/game_socket.js
@@ -72,7 +72,7 @@ SOCKET.on('win', function(msg) {
 	SOCKET.onDebugDisplay('win', msg);
 	SOCKET.state = "win";
 	alert("Vous avez gagné");
-	$().redirect('resultat.php', 
+	$.redirect('resultat.php', 
 		{'perso0': GAME.player[0].pseudo,
 		 'perso1': GAME.player[1].pseudo,
 		 'class0': GAME.player[0].pers,
@@ -86,7 +86,7 @@ SOCKET.on('loose', function(msg) {
 	SOCKET.onDebugDisplay('loose', msg);
 	SOCKET.state = "loose";
 	alert("Vous avez perdu !");
-	$().redirect('resultat.php', 
+	$.redirect('resultat.php', 
 		{'perso0': GAME.player[0].pseudo,
 		 'perso1': GAME.player[1].pseudo,
 		 'class0': GAME.player[0].pers,
